b7cdcc45cc41134e05971e02bc180b0e022ea075
shaheen5 <miyashaheen1995@gmail.com>
2021-04-22 18:33:27 +0530

    Added : Methods To Find & Display Maximum Among 3 Integers

--- main/java/com/generics/Maximum.java
+++ main/java/com/generics/Maximum.java
@@ -1,7 +1,24 @@
 package com.generics;
 
 public class Maximum {
+    //method to find maximum among three integers
+    public static Integer findMaximumInteger(Integer x, Integer y, Integer z){
+        Integer max = x;
+        if(y.compareTo(max) > 0){
+            max = y;
+        }
+        if(z.compareTo(max) > 0){
+            max = z;
+        }
+        printMaximumValue(x, y, z, max);
+        return max;
+    }
+    //method to display maximum value among three integers
+    private static void printMaximumValue(Integer x, Integer y, Integer z, Integer max){
+        System.out.println("Maximum integer value is : "+max);
+    }
     public static void main(String[] args) {
         System.out.println("Problem To Find Maximum Among Three Using Generics !");
+        findMaximumInteger(9,5,2);
     }
 }
