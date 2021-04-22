4ebacbd3b42df245b578cd604af43d0f2cdc8535
shaheen5 <miyashaheen1995@gmail.com>
2021-04-22 22:45:19 +0530

    Added : Methods To Find & Display Maximum Among 3 Strings

--- main/java/com/generics/Maximum.java
+++ main/java/com/generics/Maximum.java
@@ -33,9 +33,26 @@ public class Maximum {
     private static void printMaximumValue(Float x, Float y, Float z, Float max){
         System.out.println("Maximum float value among "+x+" , "+y+" , "+z+" is :"+max);
     }
+    //method to find maximum among three strings
+    public static String findMaximumString(String x, String y, String z){
+        String max = x;
+        if(y.compareTo(max) > 0){
+            max = y;
+        }
+        if(z.compareTo(max) > 0){
+            max = z;
+        }
+        printMaximumValue(x, y, z, max);
+        return max;
+    }
+    //method to display maximum among three strings
+    private static void printMaximumValue(String x, String y, String z, String max){
+        System.out.println("Maximum String value among "+x+" , "+y+" , "+z+" is :"+max);
+    }
     public static void main(String[] args) {
         System.out.println("Problem To Find Maximum Among Three Using Generics !");
         findMaximumInteger(9,5,2);
         findMaximumFloat(6.6f,7.8f,8.5f);
+        findMaximumString("Apple","Peach","Banana");
     }
 }
