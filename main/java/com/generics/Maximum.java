8ad6445a39c72b27c8c199c0bebf09683d6d3460
shaheen5 <miyashaheen1995@gmail.com>
2021-04-22 21:59:35 +0530

    Added : Methods To Find & Display Maximum Among 3 Floats

--- main/java/com/generics/Maximum.java
+++ main/java/com/generics/Maximum.java
@@ -15,10 +15,27 @@ public class Maximum {
     }
     //method to display maximum value among three integers
     private static void printMaximumValue(Integer x, Integer y, Integer z, Integer max){
-        System.out.println("Maximum integer value is : "+max);
+        System.out.println("Maximum integer value among "+x+" , "+y+" , "+z+" is :"+max);
+    }
+    //method to find maximum among three float values
+    public static Float findMaximumFloat(Float x, Float y, Float z){
+        Float max = x;
+        if(y.compareTo(max) > 0){
+            max = y;
+        }
+        if(z.compareTo(max) > 0){
+            max = z;
+        }
+        printMaximumValue(x, y, z, max);
+        return max;
+    }
+    //method to display maximum value among three floats
+    private static void printMaximumValue(Float x, Float y, Float z, Float max){
+        System.out.println("Maximum float value among "+x+" , "+y+" , "+z+" is :"+max);
     }
     public static void main(String[] args) {
         System.out.println("Problem To Find Maximum Among Three Using Generics !");
         findMaximumInteger(9,5,2);
+        findMaximumFloat(6.6f,7.8f,8.5f);
     }
 }
