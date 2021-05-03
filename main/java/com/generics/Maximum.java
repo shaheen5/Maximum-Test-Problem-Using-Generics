9656a2fa5f09eabf2fd2c5c928076b80c533b910
shaheen5 <miyashaheen1995@gmail.com>
2021-05-03 10:16:19 +0530

    Refactor : Code To Pass Variable Number Of Arguments & Find Maximum

--- main/java/com/generics/Maximum.java
+++ main/java/com/generics/Maximum.java
@@ -1,31 +1,40 @@
 package com.generics;
 
+import java.util.Comparator;
+import java.util.Optional;
+
+import static java.util.Arrays.*;
+
 public class Maximum <T extends Comparable<T>> {
     T x, y, z;
-    public Maximum(T x, T y, T z){
+
+    public Maximum(T x, T y, T z) {
         this.x = x;
         this.y = y;
         this.z = z;
     }
-    public T findMaximumValue(){
-        return Maximum.findMaximumValue(x, y, z);
+
+    public Maximum() {}
+
+    //method to determine largest among all
+    public Optional<T> findMaximumValue(T... parameters) {
+        Optional<T> max;
+        max = stream(parameters).sorted(Comparator.reverseOrder()).findFirst();
+        printMaximumValue(max);
+        return max;
     }
-    //method to determine largest of three comparable objects
-    public static <T extends Comparable<T>> T findMaximumValue(T x, T y, T z){
-        T max = x; //assume x is initially the largest
-        if(y.compareTo(max) > 0){
-            max = y; // y is the largest so far
-        }
-        if(z.compareTo(max) > 0){
-            max = z; //z is the largest now
-        }
-        return max; //returns the largest object
+    //method to display largest among all
+    private void printMaximumValue(Optional<T> max){
+        System.out.println("Maximum Element ="+max);
     }
+
 
     public static void main(String[] args) {
         System.out.println("Problem To Find Maximum Among Three Using Generics !");
-        findMaximumValue(9,5,2);
-        findMaximumValue(6.6f,7.8f,8.5f);
-        findMaximumValue("Apple","Peach","Banana");
+        Maximum maximum = new Maximum();
+        maximum.findMaximumValue(9, 5, 2);
+        maximum.findMaximumValue(6.6f, 7.8f, 8.5f);
+        maximum.findMaximumValue("Apple", "Peach", "Banana");
+        maximum.findMaximumValue(2, 5, 7, 1, 8);
     }
 }
--- test/java/com/generics/MaximumTest.java
+++ test/java/com/generics/MaximumTest.java
@@ -3,59 +3,61 @@ package com.generics;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Optional;
+
 public class MaximumTest {
     @Test
     public void givenMaxNumber_AtFirstPosition_ShouldReturnSameNumber() {
-        Maximum maximum=new Maximum(9,5,2);
-        Comparable maximumInteger = maximum.findMaximumValue();
-        Assert.assertEquals(9, maximumInteger);
+        Maximum maximum=new Maximum();
+        Optional<Integer> maximumInteger = maximum.findMaximumValue(9,5,2);
+        Assert.assertEquals(Optional.of(9), maximumInteger);
     }
     @Test
     public void givenMaxNumber_AtSecondPosition_ShouldReturnSameNumber() {
-        Maximum maximum=new Maximum(5,9,2);
-        Comparable maximumInteger =  maximum.findMaximumValue();
-        Assert.assertEquals(9, maximumInteger);
+        Maximum maximum=new Maximum();
+        Optional<Integer> maximumInteger = maximum.findMaximumValue(5,9,2);
+        Assert.assertEquals(Optional.of(9), maximumInteger);
     }
     @Test
     public void givenMaxNumber_AtThirdPosition_ShouldReturnSameNumber() {
-        Maximum maximum=new Maximum(2,5,9);
-        Comparable maximumInteger = maximum.findMaximumValue();
-        Assert.assertEquals(9, maximumInteger);
+        Maximum maximum=new Maximum();
+        Optional maximumInteger = (Optional) maximum.findMaximumValue(2,5,9);
+        Assert.assertEquals(Optional.of(9), maximumInteger);
     }
     @Test
     public void givenMaxFloatNumber_AtFirstPosition_ShouldReturnSameNumber() {
-        Maximum maximum=new Maximum(8.5f,6.6f,7.8f);
-        Float maximumFloat = (Float) maximum.findMaximumValue();
-        Assert.assertEquals(8.5f, maximumFloat,0.0f);
+        Maximum maximum=new Maximum();
+        Optional maximumFloat =(Optional) maximum.findMaximumValue(8.5f,6.6f,7.8f);
+        Assert.assertEquals(Optional.of(8.5f), maximumFloat);
     }
     @Test
     public void givenMaxFloatNumber_AtSecondPosition_ShouldReturnSameNumber() {
-        Maximum maximum = new Maximum(6.6f,8.5f,7.8f);
-        Float maximumFloat = (Float)maximum.findMaximumValue();
-        Assert.assertEquals(8.5f, maximumFloat,0.0f);
+        Maximum maximum = new Maximum();
+        Optional maximumFloat =(Optional) maximum.findMaximumValue(6.6f,8.5f,7.8f);
+        Assert.assertEquals(Optional.of(8.5f), maximumFloat);
     }
     @Test
     public void givenMaxFloatNumber_AtThirdPosition_ShouldReturnSameNumber() {
-        Maximum maximum = new Maximum(6.6f,7.8f,8.5f);
-        Float maximumFloat = (Float) maximum.findMaximumValue();
-        Assert.assertEquals(8.5f, maximumFloat,0.0f);
+        Maximum maximum = new Maximum();
+        Optional maximumFloat = maximum.findMaximumValue(6.6f,7.8f,8.5f);
+        Assert.assertEquals(Optional.of(8.5f), maximumFloat);
     }
     @Test
     public void givenMaxString_AtFirstPosition_ShouldReturnSameString() {
-        Maximum maximum=new Maximum("Peach","Apple","Banana");
-        Object maximumString = maximum.findMaximumValue();
-        Assert.assertEquals("Peach", maximumString);
+        Maximum maximum=new Maximum();
+        Object maximumString = maximum.findMaximumValue("Peach","Apple","Banana");
+        Assert.assertEquals(Optional.of("Peach"), maximumString);
     }
     @Test
     public void givenMaxString_AtSecondPosition_ShouldReturnSameString() {
-        Maximum maximum=new Maximum("Apple","Peach","Banana");
-        Object maximumString = maximum.findMaximumValue();
-        Assert.assertEquals("Peach", maximumString);
+        Maximum maximum=new Maximum();
+        Object maximumString = maximum.findMaximumValue("Apple","Peach","Banana");
+        Assert.assertEquals(Optional.of("Peach"), maximumString);
     }
     @Test
     public void givenMaxString_AtThirdPosition_ShouldReturnSameString() {
-        Maximum maximum=new Maximum("Apple","Banana","Peach");
-        Object maximumString = maximum.findMaximumValue();
-        Assert.assertEquals("Peach", maximumString);
+        Maximum maximum=new Maximum();
+        Object maximumString = maximum.findMaximumValue("Apple","Banana","Peach");
+        Assert.assertEquals(Optional.of("Peach"), maximumString);
     }
 }
